e3717f4bae5aae6ec085047ae8c96faa049b36ac
Johannes Schickling <schickling.j@gmail.com>
2013-06-30 15:30:38 +0200

    Init with laravel workbench

---
+++ src/Schickling/Backup/BackupServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Schickling\Backup;
+
+use Illuminate\Support\ServiceProvider;
+
+class BackupServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('schickling/backup');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
