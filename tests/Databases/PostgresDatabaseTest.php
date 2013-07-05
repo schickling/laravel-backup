d2614012cc2ea69905ddab1f7f7bcf3735545e80
Estanislau Trepat <estanis@etrepat.com>
2013-07-05 15:53:02 +0200

    Fix tests for Backup/Restore commands.

--- tests/Databases/PostgresDatabaseTest.php
+++ tests/Databases/PostgresDatabaseTest.php
@@ -23,7 +23,7 @@ class PostgresDatabaseTest extends \PHPUnit_Framework_TestCase
     public function testDump()
     {
         $this->console->shouldReceive('run')
-                      ->with('PG_PASSWORD="password" pg_dump -Fc --no-acl --no-owner -h "localhost" -U "testUser" "testDatabase" > "testfile.dump"')
+                      ->with('PGPASSWORD="password" pg_dump -Fc --no-acl --no-owner -h "localhost" -U "testUser" "testDatabase" > "testfile.dump"')
                       ->once()
                       ->andReturn(true);
 
@@ -33,7 +33,7 @@ class PostgresDatabaseTest extends \PHPUnit_Framework_TestCase
     public function testDumpFails()
     {
         $this->console->shouldReceive('run')
-                      ->with('PG_PASSWORD="password" pg_dump -Fc --no-acl --no-owner -h "localhost" -U "testUser" "testDatabase" > "testfile.dump"')
+                      ->with('PGPASSWORD="password" pg_dump -Fc --no-acl --no-owner -h "localhost" -U "testUser" "testDatabase" > "testfile.dump"')
                       ->once()
                       ->andReturn(false);
 
@@ -43,7 +43,7 @@ class PostgresDatabaseTest extends \PHPUnit_Framework_TestCase
     public function testRestore()
     {
         $this->console->shouldReceive('run')
-                      ->with('PG_PASSWORD="password" pg_restore --verbose --clean --no-acl --no-owner -h "localhost" -U "testUser" -d "testDatabase" < "testfile.dump"')
+                      ->with('PGPASSWORD="password" pg_restore --verbose --clean --no-acl --no-owner -h "localhost" -U "testUser" -d "testDatabase" "testfile.dump"')
                       ->once()
                       ->andReturn(true);
 
@@ -53,7 +53,7 @@ class PostgresDatabaseTest extends \PHPUnit_Framework_TestCase
     public function testRestoreFails()
     {
         $this->console->shouldReceive('run')
-                      ->with('PG_PASSWORD="password" pg_restore --verbose --clean --no-acl --no-owner -h "localhost" -U "testUser" -d "testDatabase" < "testfile.dump"')
+                      ->with('PGPASSWORD="password" pg_restore --verbose --clean --no-acl --no-owner -h "localhost" -U "testUser" -d "testDatabase" "testfile.dump"')
                       ->once()
                       ->andReturn(false);
 
