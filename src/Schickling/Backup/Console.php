95dae3077d2409033498fe2f6c0d0f23becfebda
Johannes Schickling <schickling.j@gmail.com>
2014-05-04 22:37:24 +0200

    Remove command logging - fixes #25

--- src/Schickling/Backup/Console.php
+++ src/Schickling/Backup/Console.php
@@ -1,21 +1,20 @@
 <?php namespace Schickling\Backup;
 
 use Symfony\Component\Process\Process;
-use Schickling\Backup\ConsoleColors;
 
 class Console
 {
 	public function run($command)
 	{
-		// Create new Colors class
-		$colors = new ConsoleColors();
-
 		$process = new Process($command);
 		$process->run();
-		echo $colors->getColoredString("\n"."Executed command : ".$command, "brown")."\n";
-		if ($process->isSuccessful()) {
+
+		if ($process->isSuccessful())
+		{
 			return true;
-		} else {
+		}
+		else
+		{
 			return $process->getErrorOutput();
 		}
 	}
